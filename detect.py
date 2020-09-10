d73719099436bc181a7c8ebd1c67504e309e993c
finn <finn.niu@apptech.com.hk>
2020-09-10 17:13:25 +0800

    init

--- detect.py
+++ detect.py
@@ -41,7 +41,7 @@ class Server(object):
 
 
     def model_rcnn(self,device,model_path):
-        device = "cuda:0"
+        device = "cpu"
         model = init_detector(self.config , model_path, device=device)
         return model
 
@@ -119,26 +119,6 @@ class Server(object):
                 # else:
                 #     sys.exit()
                 #     break
-
-
-
-if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser(description='AppTech Rodent Detection')
-    parser.add_argument(
-        '--video_path', type=str, default="@20000102013000_20000102015959_9359.mp4", help='video dir')
-    parser.add_argument(
-        '--ann_dir', type=str, default="ann/", help='bbox score threshold')
-    args = parser.parse_args()
-
-    mp.set_start_method('forkserver', force=True)
-
-    #time = (start_time, end_time,email_sent_time)
-    sever = Server(args)
-    sever.run()
-    #sever.main(cam_dir="rtsp:root:pass@192.168.1.90/axis-media/media.amp?",top=50,show=True)
-
-
 
 
 
